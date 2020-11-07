1714a8e6f50825a72b5bc824c6ad8c104856d5bc
yi.qin <yi.qin@xshoppy.com>
2020-11-07 17:30:16 +0800

    udpate .gitignore

--- src/HelloWorld/HelloWorld.php
+++ src/HelloWorld/HelloWorld.php
@@ -1,14 +1,13 @@
 <?php
 
 
-namespace src\HelloWorld;
+namespace YiTool\HelloWorld;
 
 
 class HelloWorld
 {
     public static function helloWorld()
     {
-        var_dump(123123);
         return 'hello world';
     }
 }
