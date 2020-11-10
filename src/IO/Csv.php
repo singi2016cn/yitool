b887b3bfad02130916c0621fb44378a8c21118d3
yi.qin <yi.qin@xshoppy.com>
2020-11-10 16:46:42 +0800

    新增 csv

---
+++ src/IO/Csv.php
@@ -0,0 +1,52 @@
+<?php
+
+
+namespace YiTool\IO;
+
+
+class Csv
+{
+    /**
+     * @param string $fileName
+     * @return array
+     */
+    public static function get($fileName)
+    {
+        $row = 1;
+        $res = [];
+        if (($handle = fopen($fileName, "rb")) !== FALSE) {
+            while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+                $row++;
+                $item = [];
+                $num = count($data);
+                for ($c = 0; $c < $num; $c++) {
+                    $item[] = $data[$c];
+                }
+                $res[] = $item;
+            }
+            fclose($handle);
+        }
+        return $res;
+    }
+
+    /**
+     * @param array $list
+     * @param string $fileName
+     * @return bool
+     */
+    public static function set($list, $fileName)
+    {
+        if (empty($list)) {
+            return FALSE;
+        }
+        if (empty($fileName)) {
+            return FALSE;
+        }
+        $fp = fopen($fileName, 'w+b');
+        foreach ($list as $fields) {
+            fputcsv($fp, $fields);
+        }
+        fclose($fp);
+        return TRUE;
+    }
+}
