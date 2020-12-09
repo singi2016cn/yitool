f9e43cecbefdeb90d9a0a57ffccb6d12cba2aa84
yi.qin <yi.qin@xshoppy.com>
2020-12-09 14:15:00 +0800

    新增 uuid

--- src/Common.php
+++ src/Common.php
@@ -49,4 +49,30 @@ class Common
         }
         return $res;
     }
+
+    /**
+     * 下划线转驼峰
+     * 1.原字符串转小写,原字符串中的分隔符用空格替换,在字符串开头加上分隔符
+     * 2.将字符串中每个单词的首字母转换为大写,再去空格,去字符串首部附加的分隔符.
+     * @param string $unCamelizeWords 下划线字符串
+     * @param string $separator 分隔符
+     * @return string
+     */
+    public static function camelize($unCamelizeWords, $separator = '_')
+    {
+        $unCamelizeWords = $separator . str_replace($separator, " ", strtolower($unCamelizeWords));
+        return ltrim(str_replace(" ", "", ucwords($unCamelizeWords)), $separator);
+    }
+
+    /**
+     * 驼峰命名转下划线命名
+     * 小写和大写紧挨一起的地方,加上分隔符,然后全部转小写
+     * @param string $camelCaps 驼峰命名字符串
+     * @param string $separator 转换后的分隔符
+     * @return string
+     */
+    public static function unCamelize($camelCaps, $separator = '_')
+    {
+        return strtolower(preg_replace('/([a-z])([A-Z])/', "$1" . $separator . "$2", $camelCaps));
+    }
 }
