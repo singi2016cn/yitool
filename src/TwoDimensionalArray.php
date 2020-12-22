7001b007456fe53af20b762e89c9b724cdfbe58d
yi.qin <yi.qin@xshoppy.com>
2020-12-22 14:37:45 +0800

    新增 TwoDimensionalArray.sortByValue

--- src/TwoDimensionalArray.php
+++ src/TwoDimensionalArray.php
@@ -2,6 +2,8 @@
 
 namespace YiTool;
 
+use UnexpectedValueException;
+
 class TwoDimensionalArray
 {
     /**
@@ -12,5 +14,32 @@ class TwoDimensionalArray
     public static function index0Keys($arr)
     {
         return array_keys(($arr[0] ?? []) ?: []);
+    }
+
+    /**
+     * 根据二维数组的key正序
+     * @param array $arr
+     * @param string $rowKey
+     * @param int $flag
+     * @return array
+     * @throws UnexpectedValueException
+     */
+    public static function sortByValue($arr, $rowKey, $flag = SORT_REGULAR)
+    {
+        $arrCol = array_column($arr, $rowKey);
+        if (empty($arrCol)) {
+            throw new UnexpectedValueException('rowKey not exist');
+        }
+        sort($arrCol, $flag);
+        $res = [];
+        foreach ($arrCol as $arrColItem) {
+            foreach ($arr as $item) {
+                if ($arrColItem == $item[$rowKey]) {
+                    $res[] = $item;
+                    break;
+                }
+            }
+        }
+        return $res;
     }
 }
